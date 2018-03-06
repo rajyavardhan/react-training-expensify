d06db027422715b66e625769662052feaa509f3f
rajyavardhan <rajya2020@gmail.com>
2018-03-06 22:56:25 +0530

    currency formatted to dollar

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import getExpensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/visibleExpenses';
+import numeral from 'numeraljs'
 
 export const ExpensesSummary = (props) => (
 	<div>
-		{`Viewing ${props.expenses.length} totalling ${getExpensesTotal(props.expenses)}`}
+		{`Viewing ${props.expenses.length} totalling ${numeral(getExpensesTotal(props.expenses)).format('$0.00')}`}
 	</div>	
 );
 
